25537a5bad1b37ecdc46f76a41916a28aa111648
Efe Çınar <111140694+odulsuzkisafilm@users.noreply.github.com>
2023-08-30 16:49:28 +0300

    Update IntQueue.h

--- IntQueue.h
+++ IntQueue.h
@@ -1,7 +1,7 @@
-#ifndef INTQUEUEHW6_H
-#define INTQUEUEHW6_H
+#ifndef INTQUEUE_H
+#define INTQUEUE_H
 
-class IntQueueHW6
+class IntQueue
 {
 private:
     int *queueArray;
@@ -10,14 +10,14 @@ private:
     int rear;
     int numItems;  //# of elements currently in the queue
 public:
-    IntQueueHW6(int);  //constructor, parameter is capacity
-    IntQueueHW6(const IntQueueHW6&);  //constructor, parameter is capacity
-    IntQueueHW6& operator=(const IntQueueHW6&);
+    IntQueue(int);  //constructor, parameter is capacity
+    IntQueue(const IntQueue&);  //constructor, parameter is capacity
+    IntQueue& operator=(const IntQueue&);
     void enqueue(int);
     void dequeue(int &);
     bool isEmpty() const;
     bool isFull() const;
     void clear();  //removes all elements
-    ~IntQueueHW6();
+    ~IntQueue();
 };
 #endif
